cbec685f66c7e6252168e3d40ab0a97b8a1727e6
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-14 11:06:10 -0300

    feat: change text tag with slot from radix

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,22 +1,28 @@
+import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
+import { ReactNode } from 'react';
 
 export interface TextProps{
   size?: 'sm' | 'md' | 'lg';
-  children: string;
+  children: ReactNode;
+  asChild?: boolean;
 }
 
-export function Text({size = 'md', children}: TextProps){
+export function Text({size = 'md', children, asChild}: TextProps){
+  const Comp = asChild ? Slot : 'span';
+
   return (
-    <span className={clsx(
-      'text-gray-100 font-sans',
-      {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-md': size === 'lg',
-      }
-    )}
+    <Comp 
+      className={clsx(
+        'text-gray-100 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        }
+      )}
     >
       {children}
-    </span>
+    </Comp>
   )
 }
