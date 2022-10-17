24382c3b0e1158e3df60a14efdddc59b06c046e1
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-17 12:40:35 -0300

    styles: focus style added

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -7,7 +7,7 @@ export function TextInput(props: TextInputProps){
   return (
     <input 
       className={clsx(
-        'py-4 px-3 rounded bg-gray-800 text-gray-100 text-xs placeholder:text-gray-400 w-full',
+        'py-4 px-3 rounded bg-gray-800 outline-none text-gray-100 text-xs placeholder:text-gray-400 w-full focus:ring-2 ring-cyan-300',
       )}
       {...props}
     />
