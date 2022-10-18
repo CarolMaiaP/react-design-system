a6ef270c51a56f1d48717df2d98e2d641f552d4e
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-18 17:13:36 -0300

    feat: header content added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { Heading } from './components/Heading'
+import { Text } from './components/Text'
 import { Logo } from './Logo'
 import './styles/global.css'
 
 function App() {
   return (
     <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
-      <Logo />
+      <header className='flex flex-col items-center'>
+        <Logo />
+
+        <Heading size='lg' className='mt-4'>
+          Ignite Lab
+        </Heading>
+
+        <Text size='lg' className='text-gray-400 mt-1'>Faça login e comece a usar!</Text>
+      </header>
     </div>
   )
 }
