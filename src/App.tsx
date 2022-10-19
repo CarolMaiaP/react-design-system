de946dcccf63d413e0bddc30377d1ac73fa28f6a
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-19 12:21:36 -0300

    feat: footer added

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,15 @@ function App() {
 
         <Button type='submit' className='mt-4'>Entrar na plataforma</Button>
       </form>
+      
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>Esqueceu sua senha?</a>
+        </Text>
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>Não possui conta? Crie uma agora!</a>
+        </Text>
+      </footer>
     </div>
   )
 }
