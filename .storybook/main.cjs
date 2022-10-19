f9f6ca42a4f734fdcf06cea956bc2d7663db676d
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-19 17:19:22 -0300

    feat: installing storybook interactions plugin

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -11,6 +11,7 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+    interactionsDebugger: true,
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
